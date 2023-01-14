16677f2ef5859d9b533b6881189b37e945354907
Enigma7484 <omarhaque7484@gmail.com>
2023-01-14 18:45:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sorting Algos
+ Some Python starter code for basic sorting Algorithms like MergeSort, QuickSort etc.
---
+++ bogo_sort.py
@@ -0,0 +1,17 @@
+import random
+
+
+def is_sorted(values):
+    for index in range(len(values) - 1):
+        if values[index] > values[index + 1]:
+            return False
+    return True
+
+
+def bogo_sort(values):
+    attempts = 0
+    while not is_sorted(values):
+        print(attempts)
+        random.shuffle(values)
+        attempts += 1
+    return values
